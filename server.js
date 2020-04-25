e25b98579f1d7953dc6a6c561ead9764af1d4dc2
JGPico <minq_2@hotmail.com>
2020-04-25 12:13:23 -0600

    Added users database

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const helmet = require('helmet');
 
+const authRouter = require('./auth/auth-router.js');
+const usersRouter = require('./users/users-router.js');
+const authentication = require('./auth/authenticator.js');
+
 const server = express();
 
 server.use(helmet());
 server.use(express.json());
+
+server.use('/api/auth', authRouter);
+server.use('/api/users', authentication, usersRouter);
 
 server.get('/', (req, res) => {
     res.json({Message: "The API is up!"});
