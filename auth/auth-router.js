efa75aaa4ab9eb5f489caf0e582fc97637f84bad
JGPico <minq_2@hotmail.com>
2020-04-30 11:15:44 -0600

    Testing with jest instead

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -8,13 +8,11 @@ const Users = require('../users/users-model.js');
 // for endpoints beginning with /api/auth
 router.post('/register', (req, res) => {
   let user = req.body;
-  console.log("req.body for register ", user);
   const hash = bcrypt.hashSync(user.password, 10);
   user.password = hash;
 
   Users.add(user)
     .then(saved => {
-      console.log("add user saved = ", saved);
       res.status(201).json(saved);
     })
     .catch(error => {
@@ -31,7 +29,6 @@ router.post('/login', (req, res) => {
       
       if (user && bcrypt.compareSync(password, user.password)) {
         // produce a token
-        console.log("User = ", user);
         const token = generateToken(user);
         // send the token to the client
         res.status(200).json({
@@ -40,7 +37,7 @@ router.post('/login', (req, res) => {
           token
         });
       } else {
-        res.status(401).json({ message: 'Invalid Credentials, you have a messed up token' });
+        res.status(401).json({ message: 'Invalid Credentials, you have a messed up password' });
       }
     })
     .catch(error => {
---
+++ auth/auth-router.test.js
@@ -0,0 +1,72 @@
+const request = require('supertest');
+const server = require('../server.js');
+const db = require('../database/dbConfig.js');
+
+describe('auth-router', function(){
+    const username = 'Bob';
+    const email = 'Bobs email';
+    const password = 'Bobs password';
+
+    describe('register endpoint', function(){
+        beforeEach(async () => {
+            await db('users').truncate();
+        });
+
+        it('should return 201 ok', function(){
+            return request(server)
+            .post('/api/auth/register')
+            .send({username, email, password})
+            .then(res => {
+                expect(res.status).toBe(201);
+            }); 
+        });
+
+        it('should return a user JSON', function(){
+            return request(server)
+            .post('/api/auth/register')
+            .send({username, email, password})
+            .then(res => {
+                expect(res.body.username).toBe('Bob');
+            }); 
+        });
+
+        it('should add the user to the database', async function(){
+            const existing = await db('users').where({username: "Bob"});
+            expect(existing).toHaveLength(0);
+
+            await request(server)
+            .post('/api/auth/register')
+            .send({username, email, password})
+            .then(res => {
+                expect(res.body.username).toBe('Bob');
+            }); 
+
+            const inserted = await db('users').where({username: "Bob"});
+            expect(inserted).toHaveLength(1);
+            
+        });
+    });
+
+    describe("login endpoint", function(){
+        beforeEach(async () => {
+            await db('users').truncate();
+        });
+
+        it('should return 20 ok', async function(){
+
+            await request(server)
+            .post('/api/auth/register')
+            .send({username, email, password})
+            .then(res => {
+                expect(res.body.username).toBe('Bob');
+            }); 
+
+            await request(server)
+            .post('/api/auth/login')
+            .send({username, password})
+            .then(res => {
+                expect(res.status).toBe(200);
+            }); 
+        });
+    });
+});
